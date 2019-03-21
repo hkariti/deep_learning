dd1c971c3d1a47cb0653ff37261ede48c993cda3
Hagai Kariti <hkariti@gmail.com>
2019-03-21 17:24:25 +0200

    fix finetune func

--- main.py
+++ main.py
@@ -135,8 +135,8 @@ def finetune(model):
     # Decay LR by a factor of 0.1 every 7 epochs
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
-    model = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                           num_epochs=25)
+    model = train_model(model, criterion, optimizer_ft, exp_lr_scheduler,
+                        num_epochs=25)
     return model
 
 def train_fc(model):
