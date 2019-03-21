5912f30f0dafb42819fd3c08ed447857da7ab2da
Hagai Kariti <hkariti@gmail.com>
2019-03-21 14:06:33 +0200

    add train model, put some vars in globals
    train model from https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
+from torch.autograd import Variable
 import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
@@ -12,7 +13,91 @@ import matplotlib.pyplot as plt
 
 from dataset import split_train_val
 
+dataset_sizes = {}
+dataloaders = {}
+image_datasets = {}
+device = None
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = model.state_dict()
+    best_acc = 0.0
+
+    use_gpu = torch.cuda.is_available()
+    FloatTensor = torch.cuda.FloatTensor if use_gpu else torch.FloatTensor
+    LongTensor = torch.cuda.LongTensor if use_gpu else torch.LongTensor
+    ByteTensor = torch.cuda.ByteTensor if use_gpu else torch.ByteTensor
+    Tensor = FloatTensor
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            since_epoch = time.time()
+            if phase == 'train':
+                scheduler.step()
+                model.train(True)  # Set model to training mode
+            else:
+                model.train(False)  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for data in dataloaders[phase]:
+                # get the inputs
+                inputs, labels = data
+
+                inputs = Variable(inputs.type(Tensor))
+                labels = Variable(labels.type(LongTensor))
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                outputs = model(inputs)
+                _, preds = torch.max(outputs.data, 1)
+                loss = criterion(outputs, labels)
+
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+                    
+                # statistics
+                running_loss += loss.data[0]
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(image_datasets[phase])
+            epoch_acc = running_corrects / len(image_datasets[phase])
+
+            time_elapsed_epoch = time.time() - since_epoch
+            print('{} Loss: {:.4f} Acc: {:.4f} in {:.0f}m {:.0f}s'.format(
+                phase, epoch_loss, epoch_acc, time_elapsed_epoch // 60, time_elapsed_epoch % 60))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = model.state_dict()
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
 def main():
+    global dataloaders
+    global dataset_sizes
+    global image_datasets
+    global device
     # Data augmentation and normalization for training
     # Just normalization for validation
     train_transform = transforms.Compose([
@@ -29,7 +114,6 @@ def main():
         ])
 
     data_dir = 'train'
-    image_datasets = {}
     image_datasets['train'], image_datasets['val'] = split_train_val(data_dir, train_transform=train_transform, val_transform=val_transform)
     dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
                                                  shuffle=True, num_workers=4)
