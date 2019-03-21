00526066dd2101597ef9e4091cec67a83499fb9a
Hagai Kariti <hkariti@gmail.com>
2019-03-21 13:43:42 +0200

    add main that returns dataloaders

---
+++ main.py
@@ -0,0 +1,44 @@
+import time
+import os
+import copy
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+
+from dataset import split_train_val
+
+def main():
+    # Data augmentation and normalization for training
+    # Just normalization for validation
+    train_transform = transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    val_transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
+    data_dir = 'train'
+    image_datasets = {}
+    image_datasets['train'], image_datasets['val'] = split_train_val(data_dir, train_transform=train_transform, val_transform=val_transform)
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                                 shuffle=True, num_workers=4)
+                  for x in ['train', 'val']}
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    return dataloaders
+
+if __name__ == '__main__':
+    main()
