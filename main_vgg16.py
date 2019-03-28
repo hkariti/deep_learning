5df726374c1e79598756d6f8becbba52cbac4168
Hagai Kariti <hkariti@gmail.com>
2019-03-28 17:01:36 +0200

    Add main_vgg16

---
+++ main_vgg16.py
@@ -0,0 +1,173 @@
+import time
+import os
+import copy
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+
+from .dataset import split_train_val
+
+dataset_sizes = {}
+dataloaders = {}
+image_datasets = {}
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+use_gpu = torch.cuda.is_available()
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = model.state_dict()
+    best_acc = 0.0
+
+    FloatTensor = torch.cuda.FloatTensor if use_gpu else torch.FloatTensor
+    LongTensor = torch.cuda.LongTensor if use_gpu else torch.LongTensor
+    ByteTensor = torch.cuda.ByteTensor if use_gpu else torch.ByteTensor
+    Tensor = FloatTensor
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            since_epoch = time.time()
+            if phase == 'train':
+                scheduler.step()
+                model.train(True)  # Set model to training mode
+            else:
+                model.train(False)  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for data in dataloaders[phase]:
+                # get the inputs
+                inputs, labels = data
+
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                outputs = model(inputs)
+                _, preds = torch.max(outputs.data, 1)
+                loss = criterion(outputs, labels)
+
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+                    
+                # statistics
+                running_loss += loss.data.item()
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(image_datasets[phase])
+            epoch_acc = running_corrects.type(FloatTensor) / len(image_datasets[phase])
+
+            time_elapsed_epoch = time.time() - since_epoch
+            print('{} Loss: {:.4f} Acc: {:.4f} in {:.0f}m {:.0f}s'.format(
+                phase, epoch_loss, epoch_acc, time_elapsed_epoch // 60, time_elapsed_epoch % 60))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = model.state_dict()
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+def init_data():
+    global dataloaders
+    global dataset_sizes
+    global image_datasets
+    global device
+    # Data augmentation and normalization for training
+    # Just normalization for validation
+    train_transform = transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    val_transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
+    data_dir = 'train'
+    image_datasets['train'], image_datasets['val'] = split_train_val(data_dir, train_transform=train_transform, val_transform=val_transform)
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                                 shuffle=True, num_workers=4)
+                  for x in ['train', 'val']}
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+
+    return dataloaders
+
+def finetune(model):
+    num_ftrs = model.classifier[6].in_features
+    model.classifier[6] = nn.Linear(num_ftrs, 120)
+
+    model = model.to(device)
+    criterion = nn.CrossEntropyLoss()
+
+    # Observe that all parameters are being optimized
+    optimizer_ft = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+    model = train_model(model, criterion, optimizer_ft, exp_lr_scheduler,
+                        num_epochs=25)
+    return model
+
+def train_fc(model):
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # Parameters of newly constructed modules have requires_grad=True by default
+    num_ftrs = model.classifier[6].in_features
+    model.classifier[6] = nn.Linear(num_ftrs, 120)
+
+    model = model.to(device)
+    criterion = nn.CrossEntropyLoss()
+
+    # Observe that only parameters of final layer are being optimized as
+    # opposed to before.
+    optimizer_conv = optim.SGD(model.classifier[6].parameters(), lr=0.001, momentum=0.9)
+
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+
+    model = train_model(model, criterion, optimizer_conv,
+                        exp_lr_scheduler, num_epochs=25)
+
+    return model
+
+
+def main():
+    init_data()
+
+    model = models.vgg16(pretrained=True)
+    return finetune(model)
+
+if __name__ == '__main__':
+    main()
